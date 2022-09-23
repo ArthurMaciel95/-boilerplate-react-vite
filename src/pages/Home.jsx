c8514af31a3f06efe1620abb1e17461e1bd22b8f
Arthur Rocha <62079236+ArthurMaciel95@users.noreply.github.com>
2022-09-23 12:50:49 -0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SectionQualquer from '../components/Sections/SectionQualquer'
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-center">Home</div>
+      <section>
+        <SectionQualquer />
+      </section>
+    </>
+  )
+}
+
